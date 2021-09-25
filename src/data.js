0f8cce94a7c933aaadff674755a9e348cfc7b731
Mohanenfield <83965863+Mohanenfield@users.noreply.github.com>
2021-09-25 13:32:05 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://cdn.shopify.com/s/files/1/0274/0086/3853/products/GSOMBHSSHRT2549WHTSBLU_7_StyleShoot-min_800x.jpg?v=1587081923",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://images.bewakoof.com/uploads/grid/app/types-of-shirts-for-men-bewakoof-blog-9-1610963791.jpg",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://lh3.googleusercontent.com/proxy/mFuB5x3VVV_Q0QXYLBJ_FEWYLWLcl_Ywef3uTK6JiDvepCexBhBGoVISHtIagd2aZmizvWoXmwgu10LmgFu6dvkHgNyo1fQXJrTjQ8zG3o3hD7i9gmnriYY2IyvWZbcxsBg_ygPxUOdK3ksKQCZs_OYe3ncHFcmQcpI-fg",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.unsplash.com/photo-1607345366928-199ea26cfe3e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2hpcnR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://lh3.googleusercontent.com/proxy/mFuB5x3VVV_Q0QXYLBJ_FEWYLWLcl_Ywef3uTK6JiDvepCexBhBGoVISHtIagd2aZmizvWoXmwgu10LmgFu6dvkHgNyo1fQXJrTjQ8zG3o3hD7i9gmnriYY2IyvWZbcxsBg_ygPxUOdK3ksKQCZs_OYe3ncHFcmQcpI-fg",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://content.asos-media.com/-/media/images/articles/men/2018/07/02-mon/sj-4.jpg?h=1110&w=870&la=en-AU&hash=95C71451677BD58E48CF586D21A36106",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
